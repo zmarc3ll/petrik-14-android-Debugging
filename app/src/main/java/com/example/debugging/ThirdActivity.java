1fcf9edc45f06d943c74fc1bf38a2ce2e49c66de
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-09-30 10:02:36 +0200

    initial commit, debugging

--- app/src/main/java/com/example/debugging/ThirdActivity.java
+++ app/src/main/java/com/example/debugging/ThirdActivity.java
@@ -2,23 +2,23 @@ package com.example.debugging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class ThirdActivity extends AppCompatActivity {
 
-    private Button buttonBack;
+    private Button buttonBackToMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
         init();
-        buttonBack.setOnClickListener(new View.OnClickListener() {
+        buttonBackToMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 finish();
             }
         });
@@ -26,6 +26,6 @@ public class ThirdActivity extends AppCompatActivity {
 
     public void init()
     {
-        buttonBack = findViewById(R.id.buttonExit);
+        buttonBackToMenu = findViewById(R.id.buttonBackToMenu);
     }
 }
